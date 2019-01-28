7cf895286754222f170361f65ecff3fc60bf5ffb
Lyubomir Markov <liubomir.markov@gmail.com>
2019-01-28 12:27:28 +0200

    Initial PHP Files.

---
+++ footer.php
@@ -0,0 +1,10 @@
+</div>
+        
+<!-- Popper.JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+<!-- Bootstrap JS -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+
+<script src="script/script.js"></script>
+</body>
+</html>
---
+++ models/notes.php
@@ -0,0 +1,6 @@
+<?php
+
+// TODO: Get all users notes from database;
+// var_dump($email); // Email of the user
+
+// TODO: SQL to get all users notes
