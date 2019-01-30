46788f73a8b7e49de838858da19dbaa5e0d81c49
Gogo-58bg <georgi.angelov.dl@gmail.com>
2019-01-30 15:58:01 +0200

    changes

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
             <!-- The Modal -->
           <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" id="myModal">
               <!-- TODO: Form <form> element is missing -->
-              <form action="models/notes.php" method="POST">
+              <form action="models/notes.php" method="POST" id="form">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <!-- Modal Header -->
@@ -37,16 +37,17 @@
                           >>> $_POST['mail'] // user_email
                         -->
                         <input type="hidden" name="mail" value="<?php echo $email ?>">
+                        <input type="hidden" name="id" value="" id="noteId">
                       <div class="modal-header">
-                          <input type="text" name="title" placeholder="Title" value="Title">
+                         Title: <input type="text" name="title" placeholder="Title" value="Title">
                           <button type="button" class="close" data-dismiss="modal">&times;</button>
                       </div>
                   
                       <!-- Modal body -->
                       <div class="modal-body">              
-                          <textarea name="body" class="form-control .col-xs-12 .col-sm-6 .col-lg-8" placeholder="Note"></textarea>
+                          Note:<textarea name="body" class="form-control .col-xs-12 .col-sm-6 .col-lg-8" placeholder="Note"></textarea>
                           <br>
-                          <input type="datetime" id="datepicker" name="date">
+                          Set Expiration date:<input type="datetime" id="datepicker" name="date">
                       </div>
                       <!-- TODO: Submit -->
                         <!-- Modal footer -->
@@ -62,17 +63,17 @@
           </div>
           <?php include_once('models/get_notes.php'); ?>
           <div class="notes">
+          
             <ul>
               <?php foreach ($notes as $note){ ?>
-                    
                 <li>
-                        <a href="$note" data-toggle="modal" data-target="#myModal" class="note">    
-                          <h2><?php echo $note[2]?></h2>
+                  <a href="<?php echo $note[0]?>" data-toggle="modal" data-target="#myModal" class="note">    
+                    <h2><?php echo $note[2]?></h2>
 
-                          <p><?php echo $note[3]?></p>
-                        </a>
-                      </li>
-                <?php } ?>             
+                    <p><?php echo $note[3]?></p>
+                  </a>
+                </li>
+              <?php } ?>             
             </ul>
         </div>
     </div>
--- models/notes.php
+++ models/notes.php
@@ -1,41 +1,49 @@
 <?php
-// TODO: Get all users notes from database;
-// var_dump($email); // Email of the user
-
-// TODO: SQL to get all users notes
     /**
      * On form submit.
      */
+    //nclude('./db.php');
+    //echo '<pre>';
+    //var_dump($db);
+    //die();
+    
     if (!empty($_POST)) {
         // echo '<pre>';
-        // var_dump($_POST);
+        //var_dump($_POST);
         // die();
         if (!is_null($_POST["title"])) {
             $title = $db->real_escape_string($_POST["title"]);
         }
         
-        $title = $db->real_escqpe_string($_POST["title"]);
+        //$title = $db->real_escqpe_string($_POST["title"]);
         $body = $db->real_escape_string($_POST["body"]);
-        $created = now();
+        $created = date("Y-m-d H:i:s");;
         $expired = $_POST["expired"];
-        $deleted = $_POST["id"];
-
-        $sql = "    
-            INSERT INTO 
-                notes (title,body,creted, expired, deleted, arhived) 
+       // $deleted = $_POST["id"];
+        if ($id=true){
+            $sql = "    
+            UPDATE INTO 
+                notes (title, body, expired) 
             VALUES 
                 ('$title', '$body', '$expired');
         ";
+        }
+        else {
+        $sql = "    
+            INSERT INTO 
+                notes (title, body, expired) 
+            VALUES 
+                ('$title', '$body', '$expired');
+        ";
+         }   
+        $resultNote = mysqli_query($db, $sql);
 
-        $resultArticle = mysqli_query($db, $sql);
-
-        if($resultArticle) {
+        if($resultNote) {
             echo "<em>Note added successfully!</em>";
         } else {
             echo "Error: ";
             echo '<pre>';
-            var_dump($resultArticle);
+            var_dump($resultNote);
             die();
         }
     }
-?>
