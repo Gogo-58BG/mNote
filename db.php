e11e40654de728ab2728942c345283beeedc7387
Lyubomir Markov <liubomir.markov@gmail.com>
2019-02-01 14:33:51 +0200

    Fixes and adding delete TODOs.

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-ini_set('error_reporting', 'E_COMPILE_ERROR|E_RECOVERABLE_ERROR|E_ERROR|E_CORE_ERROR');
+// ini_set('error_reporting', 'E_COMPILE_ERROR|E_RECOVERABLE_ERROR|E_ERROR|E_CORE_ERROR');
 include_once('config/db.php');
 
 //Datebase
