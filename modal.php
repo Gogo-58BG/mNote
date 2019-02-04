d1b21c86c4305eb1e8554f3a9c0a16444757c316
dep1h <b.ivan4ev@gmail.com>
2019-02-04 10:42:13 +0200

    Modal background changes

--- modal.php
+++ modal.php
@@ -2,13 +2,15 @@
 <div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" id="myModal">
 	<!-- TODO: Form <form> element is missing -->
 	<form action="models/notes.php" method="POST" id="form">
+	
 		<div class="modal-dialog" role="document">
+		<div id="created" class="created">Created: <?php echo date("Y-m-d")?></div>
 			<div class="modal-content">
 				<!-- Modal Header -->
 				<input type="hidden" name="users_email" value="<?php echo $email ?>">
 
 				<input type="hidden" name="id" value="" id="noteId">
-
+				
 				<div class="modal-header">
 					<input type="text" name="title" placeholder="Title" value="Title" class="form-control" required>
 					<button type="button" class="close" data-dismiss="modal">&times;</button>
@@ -17,7 +19,7 @@
 				<!-- Modal body -->
 				<div class="modal-body">              
 					<textarea name="body" class="form-control .col-xs-12 .col-sm-6 .col-lg-8" placeholder="Note" required rows="3" style="min-height: 100px;"></textarea>
-					<div id="created" class="created"><?php echo date("Y-m-d")?></div>
+				
 						<input type="datetime" id="datepicker" class="form-control" placeholder="Expiration" name="expired">
 					</div>
 					<!-- Modal footer -->
