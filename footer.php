70e644b46279aca50d99b4a4e9d5eeb8e1e7cb77
Andrey Kozarev <akozarev@mail.bg>
2019-02-08 16:33:40 +0200

    Some corrections.

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
 </div>
-<!-- Popper.JS -->
+
 <script src="script/popper.min.js"></script>
-<!-- Bootstrap JS -->
 <script src="script/bootstrap.min.js"></script>
 <script src="script/script.js"></script>
 <script src="script/bootstrap-password-toggler.min.js"></script>
--- header.php
+++ header.php
@@ -5,21 +5,16 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,Chrome=1" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <script src="script/html5shiv.js"></script>
-      <script type="text/javascript" src="script/respond.min.js"></script>
+    <script src="script/html5shiv.js"></script>
+    <script type="text/javascript" src="script/respond.min.js"></script>
 
     <title>Notes in Motion</title>
 
-    <!-- Bootstrap CSS CDN -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" media="screen"/>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" media="screen" />
     <link rel="stylesheet" href="css/jquery-ui.css">
-    <!-- Our Custom CSS -->
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/login.css">
-
     <link rel="stylesheet" href="css/all.css">
-    <!-- Font Awesome JS -->
     <script defer src="script/solid.js"></script>
     <script defer src="script/fontawesome.js"></script>
     <script type="text/javascript" src="script/jquery-3.1.1.js"></script>
